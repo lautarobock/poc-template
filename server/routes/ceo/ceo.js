6a9ea9aedd8de9bf90e0073b1d63ddf78ca0fb89
Lautaro <lautaromail@gmail.com>
2014-03-03 09:21:43 +0100

    mejora en el seo, y backup

--- server/routes/ceo/ceo.js
+++ server/routes/ceo/ceo.js
@@ -4,6 +4,7 @@ var robots_txt =
                 'User-agent: Googlebot-News\n' +
                 'User-agent: * \n' +
                 'Sitemap: http://www.birrasquehetomado.com.ar/sitemap.xml\n' +
+                'Sitemap: http://www.birrasquehetomado.com.ar/sitemap.html\n' +
                 'Sitemap: http://www.birrasquehetomado.com.ar/sitemap.txt'
 
 exports.configure = function(app) {
@@ -32,7 +33,7 @@ exports.configure = function(app) {
         var date = (now.getYear()+1900)+'-'+(now.getMonth()+1)+'-'+now.getDate();
         res.writeHeader(200, {"Content-Type": "text/xml"});
         res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
-        res.write(' <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
+        res.write(' <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n');
         for ( var i=1; i<=23; i++ ) {
             res.write('     <url>\n');
             var v=''+i;
@@ -47,6 +48,25 @@ exports.configure = function(app) {
         }
         
         res.write(' </urlset>\n');
+        res.end();  
+    });
+
+    app.get("/sitemap.html", function(req, res) {
+        var now = new Date();
+        var date = (now.getYear()+1900)+'-'+(now.getMonth()+1)+'-'+now.getDate();
+        res.writeHeader(200, {"Content-Type": "text/html"});
+        res.write(' <html>\n');
+        for ( var i=1; i<=23; i++ ) {
+            res.write('<div><a title="Categoria ' + v + '" href="');
+            var v=''+i;
+            if ( i<10) {
+                v='0'+i;
+            }
+            res.write('html/#/beer?%5Bcategory%5D='+v+'">');
+            res.write('Categoria ' + v + '</a></div>\n');
+        }
+        
+        res.write(' </html>\n');
         res.end();  
     });
 
